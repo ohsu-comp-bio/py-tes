a1651be0a5b1ca4c250e7a07b09dcfe7741dd110
adamstruck <strucka@ohsu.edu>
2017-07-13 09:59:42 -0700

    update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ SETUP_DIR = os.path.dirname(__file__)
 README = os.path.join(SETUP_DIR, 'README.md')
 
 setup(
-    name='py-tes',
+    name='tes',
     version='0.1',
     description='Library for communicating with the GA4GH Task Execution API',
     long_description=open(README).read(),
@@ -21,6 +21,9 @@ setup(
     download_url="https://github.com/ohsu-comp-bio/py-tes",
     license='MIT',
     install_requires=[
+        "attrs>=17.2.0",
+        "polling>=0.3.0",
+        "requests>=2.18.1",
     ],
     zip_safe=True
 )
