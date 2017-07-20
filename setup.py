1f2d77906b3f228335bc8829cf3459e205676479
adamstruck <strucka@ohsu.edu>
2017-07-20 09:21:51 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@
 
 from __future__ import print_function
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='tes',
+    name='py-tes',
     version='0.1.0',
     description='Library for communicating with the GA4GH Task Execution API',
     author='OHSU Computational Biology',
+    author_email='CompBio@ohsu.edu',
+    maintainer='Adam Struck',
     maintainer_email='strucka@ohsu.edu',
     url="https://github.com/ohsu-comp-bio/py-tes",
     license='MIT',
+    packages=find_packages(),
+    python_requires='>=2.6, <3',
     install_requires=[
         "attrs>=17.2.0",
         "polling>=0.3.0",
