6ddf44af775533cd9e69ac0c119223945b81252e
adamstruck <strucka@ohsu.edu>
2017-07-27 14:02:43 -0700

    version bump

--- tes/__init__.py
+++ tes/__init__.py
@@ -1,19 +1,17 @@
 from __future__ import absolute_import, print_function
 
 from tes.client import HTTPClient
-from tes.models import (TaskParameter, Resources, Ports, Executor, ExecutorLog,
-                        OutputFileLog, TaskLog, Task, ServiceInfo,
-                        ListTasksResponse)
+from tes.models import (TaskParameter, Resources, Ports, Executor, Task,
+                        ServiceInfo, ListTasksResponse)
 __all__ = [
     HTTPClient,
     TaskParameter,
     Resources,
     Ports,
     Executor,
-    ExecutorLog,
-    OutputFileLog,
-    TaskLog,
     Task,
     ServiceInfo,
     ListTasksResponse
 ]
+
+__version__ = "0.1.5"
