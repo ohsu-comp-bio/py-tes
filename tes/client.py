f2e69099c928de05357e1b3e55e867f8f189a879
Liam Beckman <lbeckman314@gmail.com>
2024-11-01 13:02:32 -0700

    Release 1.1.0 :tada: (#57)
    * feat: support Python 3.7+; drop Python 2.7,<3.7
    
    * feat: pass through json serialization options
    
    * docs: add advanced usage examples
    
    * feat: spec-compliant routes, with legacy support
    
    * fix: remove debug messages
    
    * test: increase unit test coverage (#52)
    
    * test: increase unit test coverage
    
    * Update tests.yml
    
    ---------
    
    Co-authored-by: Liam Beckman <lbeckman314@gmail.com>
    
    * docs: browsable API reference on GitHub Pages (#49)
    
    * docs: browsable API reference on GitHub Pages
    
    * Update tests.yml
    
    ---------
    
    Co-authored-by: Liam Beckman <lbeckman314@gmail.com>
    
    * Minor release testing updates
    
    * Update support for Service Info (#55)
    
    - Fix version to match PyPi release (#59)
    
    * Add integration test with Funnel
    
    * Add initial TES integration test (Funnel)
    
    * Add Tmate debug session
    
    * Update Funnel installation
    
    * Re-add all unit tests
    
    * Fix Funnel download issue with correct rc version
    
    * Add latest stable Python to test matrix
    
    * Fix unit tests not reaching Funnel
    
    * Increase test coverage
    
    * Minor linting fix
    
    * Update raised exception for invalid server response
    
    * Update README
    
    * Update README.md
    
    * Exclude tests from packages (#58)
    
    * Add in 3 fields missing in the models for tes.
    
    ---------
    
    Co-authored-by: Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
    Co-authored-by: Kyle Ellrott <kellrott@gmail.com>
    Co-authored-by: Ben Beasley <code@musicinmybrain.net>
    Co-authored-by: Venkat Malladi <vmalladi@microsoft.com>

--- tes/client.py
+++ tes/client.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, print_function, unicode_literals
+"""TES access methods and helper functions."""
 
 import re
 import requests
@@ -6,13 +6,91 @@ import time
 
 from attr import attrs, attrib
 from attr.validators import instance_of, optional
-from builtins import str
-from requests.utils import urlparse
+from urllib.parse import urlparse
+from typing import Any, Dict, List, Optional
 
 from tes.models import (Task, ListTasksRequest, ListTasksResponse, ServiceInfo,
                         GetTaskRequest, CancelTaskRequest, CreateTaskResponse,
                         strconv)
 from tes.utils import unmarshal, TimeoutError
+
+
+def append_suffixes_to_url(
+    urls: List[str], suffixes: List[str]
+) -> List[str]:
+    """Compile all combinations of full paths from paths and suffixes.
+
+    Args:
+        urls: List of URL paths.
+        prefixes: List of suffixes to be appended to `urls`.
+
+    Returns:
+        List of full path combinations, in the provided order of `paths` and
+        `suffixes`, starting with all suffix combinations for the first path,
+        then those for the second path, and so on. Paths are stripped of any
+        trailing slashes.
+
+    Examples:
+        >>> client = tes.HTTPClient.append_suffixes_to_url(['https://funnel.exa
+        mple.com'], ['ga4gh/tes/v1', 'v1', ''])
+        ['https://funnel.example.com/ga4gh/tes/v1', 'https://funnel.example.com
+        /v1', 'https://funnel.example.com']
+    """
+    compiled_paths: List[str] = []
+    for url in urls:
+        for suffix in suffixes:
+            compiled_paths.append(
+                f"{url.rstrip('/')}/{suffix.strip('/')}".rstrip('/'))
+    return compiled_paths
+
+
+def send_request(
+    paths: List[str], method: str = 'get',
+    kwargs_requests: Optional[Dict[str, Any]] = None, **kwargs: Any
+) -> requests.Response:
+    """Send request to a list of URLs, returning the first valid response.
+
+    Args:
+        paths: List of fully qualified URLs.
+        method: HTTP method to use for the request; one of 'get' (default),
+            'post', 'put', and 'delete'.
+        kwargs_requests: Keyword arguments to pass to the :mod:`requests` call.
+        **kwargs: Keyword arguments for path parameter substition.
+
+    Returns:
+        The first successful response from the list of endpoints.
+
+    Raises:
+        requests.exceptions.HTTPError: If no response is received from any
+            path.
+        requests.exceptions.HTTPError: As soon as the first 4xx or 5xx status
+            code is received.
+        requests.exceptions.HTTPError: If, after trying all paths, at least one
+            404 status code and no other 4xx or 5xx status codes are received.
+        ValueError: If an unsupported HTTP method is provided.
+    """
+    if kwargs_requests is None:
+        kwargs_requests = {}
+    if method not in ('get', 'post', 'put', 'delete'):
+        raise ValueError(f"Unsupported HTTP method: {method}")
+
+    response: requests.Response = requests.Response()
+    http_exceptions: Dict[str, Exception] = {}
+    for path in paths:
+        try:
+            response = getattr(requests, method)(
+                path.format(**kwargs), **kwargs_requests)
+        except requests.exceptions.RequestException as exc:
+            http_exceptions[path] = exc
+            continue
+        if response.status_code != 404:
+            break
+
+    if response.status_code is None:
+        raise requests.exceptions.HTTPError(
+            f"No response received; HTTP Exceptions: {http_exceptions}")
+    response.raise_for_status()
+    return response
 
 
 def process_url(value):
@@ -21,20 +99,35 @@ def process_url(value):
 
 @attrs
 class HTTPClient(object):
-    url = attrib(converter=process_url)
-    timeout = attrib(default=10, validator=instance_of(int))
-    user = attrib(default=None,
-                  converter=strconv,
-                  validator=optional(instance_of(str)))
-    password = attrib(default=None,
-                      converter=strconv,
-                      validator=optional(instance_of(str)))
-    token = attrib(default=None,
-                   converter=strconv,
-                   validator=optional(instance_of(str)))
+    """HTTP client class for interacting with the TES API."""
+    url: str = attrib(converter=process_url, validator=instance_of(str))
+    timeout: int = attrib(default=10, validator=instance_of(int))
+    user: Optional[str] = attrib(
+        default=None, converter=strconv, validator=optional(instance_of(str)))
+    password: Optional[str] = attrib(
+        default=None, converter=strconv, validator=optional(instance_of(str)))
+    token: Optional[str] = attrib(
+        default=None, converter=strconv, validator=optional(instance_of(str)))
 
-    @url.validator
+    def __attrs_post_init__(self):
+        # for backward compatibility
+        self.urls: List[str] = append_suffixes_to_url(
+            [self.url], ["/ga4gh/tes/v1", "/v1", "/"]
+        )
+
+    @url.validator  # type: ignore
     def __check_url(self, attribute, value):
+        """Validate URL scheme of TES instance.
+
+        `attrs` validator function for `HTTPClient.url`.
+
+        Args:
+            attribute: Attribute being validated.
+            value: Attribute value.
+
+        Raises:
+            ValueError: If URL scheme is unsupported.
+        """
         u = urlparse(value)
         if u.scheme not in ["http", "https"]:
             raise ValueError(
@@ -42,48 +135,87 @@ class HTTPClient(object):
                 % ("http", "https")
             )
 
-    def get_service_info(self):
-        kwargs = self._request_params()
-        response = requests.get(
-            "%s/v1/tasks/service-info" % (self.url),
-            **kwargs)
-        response.raise_for_status()
+    def get_service_info(self) -> ServiceInfo:
+        """Access method for `GET /service-info`.
+
+        Returns:
+            `tes.models.ServiceInfo` instance.
+        """
+        kwargs: Dict[str, Any] = self._request_params()
+        paths = append_suffixes_to_url(
+            self.urls, ["service-info", "tasks/service-info"]
+        )
+        response = send_request(paths=paths, kwargs_requests=kwargs)
         return unmarshal(response.json(), ServiceInfo)
 
-    def create_task(self, task):
+    def create_task(self, task: Task) -> CreateTaskResponse:
+        """Access method for `POST /tasks`.
+
+        Args:
+            task: `tes.models.Task` instance.
+
+        Returns:
+            `tes.models.CreateTaskResponse` instance.
+
+        Raises:
+            TypeError: If `task` is not a `tes.models.Task` instance.
+        """
         if isinstance(task, Task):
             msg = task.as_json()
         else:
             raise TypeError("Expected Task instance")
 
-        kwargs = self._request_params(data=msg)
-        response = requests.post(
-            "%s/v1/tasks" % (self.url),
-            **kwargs
-        )
-        response.raise_for_status()
+        kwargs: Dict[str, Any] = self._request_params(data=msg)
+        paths = append_suffixes_to_url(self.urls, ["/tasks"])
+        response = send_request(paths=paths, method='post',
+                                kwargs_requests=kwargs)
         return unmarshal(response.json(), CreateTaskResponse).id
 
-    def get_task(self, task_id, view="BASIC"):
-        req = GetTaskRequest(task_id, view)
-        payload = {"view": req.view}
-        kwargs = self._request_params(params=payload)
-        response = requests.get(
-            "%s/v1/tasks/%s" % (self.url, req.id),
-            **kwargs)
-        response.raise_for_status()
+    def get_task(self, task_id: str, view: str = "BASIC") -> Task:
+        """Access method for `GET /tasks/{id}`.
+
+        Args:
+            task_id: TES Task ID.
+            view: Task info verbosity. One of `MINIMAL`, `BASIC` and `FULL`.
+
+        Returns:
+            `tes.models.Task` instance.
+        """
+        req: GetTaskRequest = GetTaskRequest(task_id, view)
+        payload: Dict[str, Optional[str]] = {"view": req.view}
+        kwargs: Dict[str, Any] = self._request_params(params=payload)
+        paths = append_suffixes_to_url(self.urls, ["/tasks/{task_id}"])
+        response = send_request(paths=paths, kwargs_requests=kwargs,
+                                task_id=req.id)
         return unmarshal(response.json(), Task)
 
-    def cancel_task(self, task_id):
-        req = CancelTaskRequest(task_id)
-        kwargs = self._request_params()
-        response = requests.post(
-            "%s/v1/tasks/%s:cancel" % (self.url, req.id),
-            **kwargs)
-        response.raise_for_status()
-        return
+    def cancel_task(self, task_id: str) -> None:
+        """Access method for `POST /tasks/{id}:cancel`.
 
-    def list_tasks(self, view="MINIMAL", page_size=None, page_token=None):
+        Args:
+            task_id: TES Task ID.
+        """
+        req: CancelTaskRequest = CancelTaskRequest(task_id)
+        kwargs: Dict[str, Any] = self._request_params()
+        paths = append_suffixes_to_url(self.urls, ["/tasks/{task_id}:cancel"])
+        send_request(paths=paths, method='post', kwargs_requests=kwargs,
+                     task_id=req.id)
+        return None
+
+    def list_tasks(
+        self, view: str = "MINIMAL", page_size: Optional[int] = None,
+        page_token: Optional[str] = None
+    ) -> ListTasksResponse:
+        """Access method for `GET /tasks`.
+
+        Args:
+            view: Task info verbosity. One of `MINIMAL`, `BASIC` and `FULL`.
+            page_size: Number of tasks to return.
+            page_token: Token to retrieve the next page of tasks.
+
+        Returns:
+            `tes.models.ListTasksResponse` instance.
+        """
         req = ListTasksRequest(
             view=view,
             page_size=page_size,
@@ -91,47 +223,55 @@ class HTTPClient(object):
             name_prefix=None,
             project=None
         )
-        msg = req.as_dict()
+        msg: Dict = req.as_dict()
 
-        kwargs = self._request_params(params=msg)
-        response = requests.get(
-            "%s/v1/tasks" % (self.url),
-            **kwargs)
-        response.raise_for_status()
+        kwargs: Dict[str, Any] = self._request_params(params=msg)
+        paths = append_suffixes_to_url(self.urls, ["/tasks"])
+        response = send_request(paths=paths, kwargs_requests=kwargs)
         return unmarshal(response.json(), ListTasksResponse)
 
-    def wait(self, task_id, timeout=None):
-        def check_success(data):
+    def wait(self, task_id: str, timeout=None) -> Task:
+        def check_success(data: Task) -> bool:
             return data.state not in ["QUEUED", "RUNNING", "INITIALIZING"]
 
         max_time = time.time() + timeout if timeout else None
 
+        response: Optional[Task] = None
         while True:
             try:
                 response = self.get_task(task_id, "MINIMAL")
             except Exception:
-                response = None
+                raise Exception(f"Failed to get task {task_id}")
 
             if response is not None:
                 if check_success(response):
                     return response
 
-            if max_time is not None and time.time() >= max_time:
-                raise TimeoutError("last_response: %s" % (response.as_dict()))
+                if max_time is not None and time.time() >= max_time:
+                    raise TimeoutError("last_response: {response.as_dict()}")
             time.sleep(0.5)
 
-    def _request_params(self, data=None, params=None):
-        kwargs = {'timeout': self.timeout}
+    def _request_params(
+        self, data: Optional[str] = None, params: Optional[Dict] = None
+    ) -> Dict[str, Any]:
+        """Compile request parameters.
 
+        Args:
+            data: JSON payload to be sent in the request body.
+
+        Returns:
+            Dictionary of request parameters.
+        """
+        kwargs: Dict[str, Any] = {}
+        kwargs['timeout'] = self.timeout
+        kwargs['headers'] = {}
+        kwargs['headers']['Content-type'] = 'application/json'
+        if self.user is not None and self.password is not None:
+            kwargs['auth'] = (self.user, self.password)
         if data:
             kwargs['data'] = data
         if params:
             kwargs['params'] = params
         if self.token:
-            kwargs['headers'] = {'Content-type': 'application/json',
-                                 'Authorization': 'Bearer ' + self.token}
-        else:
-            kwargs['headers'] = {'Content-type': 'application/json'}
-            kwargs['auth'] = (self.user, self.password)
-
+            kwargs['headers']['Authorization'] = f"Bearer {self.token}"
         return kwargs
